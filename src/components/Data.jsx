dd73ccf5e830150f3cbd682fad70387b5a2b2401
Annette <annette.ngugi@student.moringaschool.com>
2025-04-14 20:23:56 +0300

    complete data.jsx

---
+++ src/components/Data.jsx
@@ -0,0 +1,45 @@
+const expensesData = [
+    {
+      id: 1,
+      expense: "Fries and Chicken",
+      description: "Tuesday's Lunch",
+      category: "Food",
+      amount: 200,
+      date: "2022-12-02"
+    },
+    {
+      id: 2,
+      expense: "KPLC Tokens",
+      description: "Power tokens",
+      category: "Electricity",
+      amount: 400,
+      date: "2022-04-03"
+    },
+    {
+      id: 3,
+      expense: "Buy Shoes",
+      description: "Add to my shoe collection",
+      category: "Personal",
+      amount: 1700,
+      date: "2022-05-05"
+    },
+    {
+      id: 4,
+      expense: "Buy Book",
+      description: "Add to my book collection",
+      category: "School",
+      amount: 700,
+      date: "2022-11-19"
+    },
+    {
+      id: 5,
+      expense: "Pay Loan",
+      description: "Loan repayment",
+      category: "Finance",
+      amount: 30000,
+      date: "2022-07-18"
+    }
+  ];
+  
+  export default expensesData;
+  
